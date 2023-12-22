c40aed9a3172dae03a438f205fab15cd5e578f51
astiannta14 <153782825+astiannta14@users.noreply.github.com>
2023-12-22 21:53:23 +0700

    Upload File c7

---
+++ praktikum-c7-231031083.py
@@ -0,0 +1,22 @@
+import os
+os.system('clear')
+
+pwd_benar = 'si23c'
+a = True
+limit = 3
+i=0
+
+while a:
+    i += 1
+    pwd = input('Masukkan password: ')
+    if pwd == pwd_benar:
+        print('Selamat anda berhasil login!')
+        a = False
+    else:
+        if i < limit:
+            print('Password salah! coba lagi.')
+            a = True
+        else:
+            print('Kesempatan anda habis!')
+            a=False
+
